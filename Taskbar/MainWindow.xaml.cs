a9a6779b075a4c52eb206ba01a74ed00cf26d46a
Mathieu Boily <mathiboily@hotmail.com>
2019-06-12 21:34:27 -0400

    Ajout bouton taskmanager

--- Taskbar/MainWindow.xaml.cs
+++ Taskbar/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,27 +23,42 @@ namespace Taskbar
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<ApplicationButton> AppButtons = new List<ApplicationButton>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            AppButtons.Add(new ApplicationButton { Button = btnTaskManager, IconPath = @"C:\Windows\system32\taskmgr.exe" });
 
             Background = Brushes.Gray;
             Height = SystemParameters.WorkArea.Height;
             Top = 0;
             Left = 0;
+
+            SetupAppButtons();
+        }
+
+        private void SetupAppButtons()
+        {
+            int btnCount = 0;
+            foreach (ApplicationButton button in AppButtons)
+            {
+                var icon = System.Drawing.Icon.ExtractAssociatedIcon(@"C:\Windows\system32\taskmgr.exe");
+                button.Button.Content = new Image { Source = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()) };
+                button.Button.Width = 30;
+                button.Button.Height = 30;
+                button.Button.Margin = new Thickness(btnCount * 30, SystemParameters.WorkArea.Height - 30, 0, 0);
+
+                btnCount++;
+            }
         }
 
         private void BtnIisResetFlushTemp_Click(object sender, RoutedEventArgs e)
         {
             btnIisResetFlushTemp.IsEnabled = false;
 
-            Process process = new Process();
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.FileName = "IISReset.exe";
-            process.Start();
-
+            Process process = StartProcess("IISReset.exe");
             process.WaitForExit();
 
             Directory.Delete(@"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\root", true);
@@ -58,6 +74,23 @@ namespace Taskbar
         private void Window_MouseLeave(object sender, MouseEventArgs e)
         {
             Width = 10;
+        }
+
+        private void BtnTaskManager_Click(object sender, RoutedEventArgs e)
+        {
+            StartProcess("taskmgr.exe");
+        }
+
+        private Process StartProcess(string processPath)
+        {
+            Process process = new Process();
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = processPath;
+            process.Start();
+
+            return process;
         }
     }
 }
